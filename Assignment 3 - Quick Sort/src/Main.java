28f589aeea7d9bdc6f2ec2df01bb92bd0b7a155e
willychang21 <willychang17@gmail.com>
2023-02-25 06:02:06 -0800

    update assignment3

--- Assignment 3 - Quick Sort/src/Main.java
+++ Assignment 3 - Quick Sort/src/Main.java
@@ -2,15 +2,55 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-            Problem 1
-            Given a set of intervals, return true if a person can make all of the meetings or false if one person cannot. Intervals[i] = [start, end]. Please see the examples below
-         */
-        System.out.println("Hello world!");
+        Problem 1
+        Given a set of intervals, return true if a person can make all meetings or false if one person cannot. Intervals[i] = [start, end]. Please see the examples below
+        */
+
+        /*
+        Example 1
+        Input: intervals = [[0,30],[5,10],[15,20]]
+        Output: false
+        */
+
+        int[][] input1 = {{ 0, 30 }, { 5, 10 }, { 15, 20 }};
+        System.out.println(sort.canAttendMeetings(input1));
+
+        /*
+        Example 2
+        Input: intervals = [[7,10],[2,4]]
+        Output: true
+        */
+        int[][] input2 = {{ 7, 10 }, { 2, 4 }};
+        System.out.println(sort.canAttendMeetings(input2));
+
+        /*
+        Problem 2
+        You are given a binary array (nums) containing only integers 1 and 0s.  Return the number of sub arrays in the whole array where there are more 1's than 0's.
+        */
+
+        /*
+        Example 1
+        Input: intervals = [0,1,1,0,1]
+        Output: 9
+        */
+        int[] input3 = {0,1,1,0,1};
+        System.out.println(sort.numSubArraysWithMoreOnes(input3));
 
 
         /*
-            Problem 2
-            You are given a binary array (nums) containing only integers 1 and 0s.  Return the number of subarrays in the whole array where there are more 1's than 0's.
-         */
+        Example 2
+        Input: intervals = [0]
+        Output: 0
+        */
+        int[] input4 = {0};
+        System.out.println(sort.numSubArraysWithMoreOnes(input4));
+
+        /*
+        Example 3
+        Input: intervals = [1]
+        Output: 1
+        */
+        int[] input5 = {1};
+        System.out.println(sort.numSubArraysWithMoreOnes(input5));
     }
 }
--- Assignment 3 - Quick Sort/src/sort.java
+++ Assignment 3 - Quick Sort/src/sort.java
@@ -1,43 +1,82 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class sort {
     /**
-     * @param nums an integer array
-     * @return void
+     * Problem 1
+     * Given a set of intervals, return true if a person can make all meetings or false if one person cannot. Intervals[i] = [start, end]. Please see the examples below
+     * @param intervals a 2D integer array
+     * @return boolean
+     * Time Complexity: O(NlogN)
+     * Space Complexity: O(logN)
      */
-    public void quickSort(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
+    public static boolean canAttendMeetings(int[][] intervals) {
+        // Sort the intervals array using quicksort
+        quicksort(intervals, 0, intervals.length-1);
+
+        // Iterate through the intervals array and check for overlaps
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i-1][1]) {
+                return false;
+            }
+        }
+        // If we reach the end of the loop without finding any overlaps,
+        // return true
+        return true;
     }
 
-    private void quickSort(int[] nums, int start, int end) {
-        if (start >= end) {
-            return;
+    // Define a quicksort function to sort the intervals array
+    private static void quicksort(int[][] intervals, int l, int r) {
+        if (l < r) {
+            int pi = partition(intervals, l, r);
+            quicksort(intervals, l, pi-1);
+            quicksort(intervals, pi+1, r);
         }
+    }
 
-        int left = start;
-        int right = end;
-        int mid = start + (end - start) / 2;
-        int pivot = nums[mid];
-
-        // overview sorted
-        while (left <= right) {
-            while (left <= right && nums[left] < pivot) {
-                left++;
-            }
-
-            while (left <= right && nums[right] > pivot) {
-                right--;
-            }
-
-            // swap left >= p with right <= p to make left <= p right >= p
-            if (left <= right) {
-                int tmp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = tmp;
-                left++;
-                right--;
+    // Define a partition function to partition the intervals array
+    private static int partition(int[][] intervals, int l, int r) {
+        int[] pivot = intervals[r];
+        int i = l;
+        for (int j = l; j < r; j++) {
+            if (intervals[j][0] < pivot[0]) {
+                int[] temp = intervals[i];
+                intervals[i] = intervals[j];
+                intervals[j] = temp;
+                i++;
             }
         }
+        int[] temp = intervals[i];
+        intervals[i] = intervals[r];
+        intervals[r] = temp;
+        return i;
+    }
 
-        quickSort(nums, start, right);
-        quickSort(nums, left, end);
+    /**
+     * Problem 2
+     * You are given a binary array (nums) containing only integers 1 and 0s.  Return the number of sub arrays in the whole array where there are more 1's than 0's.
+     * @param nums an integer array
+     * @return integer
+     * Time Complexity: O(N)
+     * Space Complexity: O(N)
+     */
+
+    // using DP: assume the end of subarray index i-1 has 1 more than 0, then can derive the answer at i
+    public static int numSubArraysWithMoreOnes(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int arrCount = 0, result = 0, count = 0;
+        for(int i : nums){
+            if(i == 1) {
+                arrCount += map.getOrDefault(count, 0);
+            }
+            else {
+                arrCount -= map.getOrDefault(count - 1, 0);
+            }
+            result += arrCount;
+            count += i == 0 ? -1 : 1;
+            map.put(count, map.getOrDefault(count, 0) + 1);
+        }
+        return result;
     }
 }
